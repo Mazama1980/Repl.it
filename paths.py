d861a39e05a0e5737c3bda9c2725fb8ac59835e6
Mazama1980 <74572218+Mazama1980@users.noreply.github.com>
2021-03-22 19:10:42 -0600

    Canonical Path exercises

--- paths.py
+++ paths.py
@@ -1,1 +1,28 @@
 
+from pathlib import Path
+
+def main():
+    path = Path("data/contacts.txt")
+    if not path.exists():
+        print("Sorry, no file named contacts.txt")
+        return
+
+    if not path.is_file():
+        print("Unable to read file contacts.txt")
+        return
+
+    fh = open(path)
+    contents = fh.read()
+    fh.close()
+    print(contents)
+
+def groceries():
+    food = Path(__file__).parent / "data" / "groceries.txt"
+    
+    fh = open(food)
+    with open(food):
+        contents = fh.read()
+        print(contents)
+
+main()
+groceries()
