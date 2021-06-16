e090190bbcc88f6f73edc164740684d9f9010a53
Mazama1980 <74572218+Mazama1980@users.noreply.github.com>
2021-06-15 19:16:22 -0600

    flashcards exersizes 7-9
    Co-authored-by: Alissa Huskey <alissa.huskey@gmail.com>
    Co-authored-by: siporter43 <siporter43@users.noreply.github.com>

--- flashcards.py
+++ flashcards.py
@@ -7,75 +7,25 @@ https://alissa-huskey.github.io/python-class/exercises/flashcards.html
 [x] Part 3: start load_csv()
 [x] Part 4: Read each line of the csv file
 [x] Part 5: Get the card data from the csv file
-[ ] Part 6: Return the card data to main()
-[ ] Part 7: Remove extra whitespace
+[x] Part 6: Return the card data to main()
+[x] Part 7: Remove extra whitespace
+[x] Part 8: Skip the header row
+[ ] Part 9: Start the play() function¶
 
-    The load_csv() function should take one argument, a Path object to a csv
-    file. Eventually it will return a list where each item is a dict with
-    "front" and "back" keys, one for each row in the file except the header.
+    This function should take one argument, the list of cards. Eventually it
+    will contain the user interface for running through each card, getting the
+    answers from the user, and printing the score.
 
-    For now though, make a simple function that just takes a Path object, prints
-    the location, and makes sure the file exists.
+    For now we'll just write a play() function and call it.
 
-    load_csv()
+    play()
+    [x] write a play() function that takes one argument: cards
+    [x] for temporary debugging, print something from it
 
-        [x] write a load_csv() function that takes one argument: path
-        [x] check to make sure the csv file exists. If not, print an error
-            message that includes the path then return
-        [x] print a temporary debug message: "loading file: path"
+    main()
+    [ ] call play() passing it the list of cards
 
-    in main()
-
-        [x] make a Path object to your csv file
-        [x] call your load_csv() function, passing it your Path object as the
-            argument
-        [x] and assign the returned value to a variable named cards
-
-    in load_csv()
-
-        [x] open the csv file in read mode using the open() function
-        [x] use fp.readlines() to iterate through each line in the file
-        [x] for temporary debugging, print each line
-
-    in load_csv(), in the readlines() loop
-
-        [x] make an empty dict assigned to a variable named card
-        [x] split each line on the "," using the .split() method and assign the
-            result to a variable named row
-        [x] check that there are two items in the row using the len() function.
-            If not print an error message and return
-        [x] assign card["front"] to the first item in the row, and card["back"]
-            to the second
-        [x] for temporary debugging, print the card dict
-
-
-    Have load_csv() put all of the card dictionaries into one big cards list and return that to main().
-
-    in load_csv(), before the readlines() loop
-
-        [x] make an empty list assigned to a variable named cards
-            in load_csv(), at the end of the readlines() loop
-        [x] use the .append() method on the cards list with the argument card
-            in load_csv(), after the loop
-        [x] return cards
-
-        in main()
-
-        [ ] if the cards list is falsy, return
-        [ ] otherwise, print the cards list for temporary debugging
-
-        
-
-    In order to make sure that the flashcards are printed nicely and that the
-    users answers get matched up correctly, the trailing "\n" needs to be
-    removed from the end of each line. Any extra spaces that happen to be around
-    the "," or at the beginning or end of the line also need to be removed.
-
-    in load_csv(), in the loop
-
-        [ ] remove leading and trailing whitespace by calling the .strip()
-            method on card["front"] and card["back"]
-  """
+"""
 # ### Imports ################################################################
 
 from pathlib import Path
@@ -103,12 +53,17 @@ def load_csv(path):
             return
         card["front"] = row[0] 
         card["back"] = row[1]
+        card["front"] = card["front"].strip()
+        card["back"] = card["back"].strip()
         cards.append(card)
         print(card)
     fp.close()
     return(cards)
 
 
+def play(cards):
+    print("Hello from the Play Function")
+    
 
 # The main() function should be at the last function defined
 #
@@ -122,6 +77,7 @@ def main():
         return
     else:
         print(cards)
+    play(cards)
     
 
 # ## Runner ##################################################################
