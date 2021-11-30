86a618b1b03284c165a3286525ad01fd8eda1290
Mazama1980 <74572218+Mazama1980@users.noreply.github.com>
2021-11-29 19:44:52 -0700

    Review 2021-11-29
    Co-authored-by: Alissa Huskey <alissa.huskey@gmail.com>
    Co-authored-by: siporter43 <siporter43@users.noreply.github.com>

--- dictionaries.py
+++ dictionaries.py
@@ -22,4 +22,54 @@ def objects():
         print("The shape", answer, "has", sides, "sides.")
     else:
         print("Sorry I do not know that shape:", answer)
-objects()
+#objects()
+
+"""2021-11-29 
+
+Attendees
+- Nila
+- Sean
+
+"""
+
+#######################################################################
+# Order of Operation Rules
+#
+# - replace variables with values
+# - */ before +-
+# - inside to outside
+# - left to right (except before =)
+#
+# Expression - any piece of code that evaluates to a value
+#
+#######################################################################
+
+# 1. Make a list of seasons (lowercase). Print the first three characters of the 3rd season, title cased.
+# 2. Make a dictionary where the key is the numbers 1-7 and the value is the days of the week: monday - sunday.
+#    - Write a function that takes one argument, a number, and returns the day of the week associated with that number.
+# 3. Make a dictionary named schedule where the key is a weekday and the value is another dictionary. 
+#    - The inner dictionary should be the schedule for the day, where the key is the time in 24 hour format (ie: "14:30")
+#      and the value is the thing scheduled
+#    - Use a for loop to iterate over each day and print the day name,
+#    - then use a nested for loop to iterate over the schedule and print the time and activity
+
+def year():
+    seasons = ["spring", "summer", "fall", "winter"]
+    season = seasons[2][0:3].capitalize()
+    print(season)
+year()
+
+weekdays = {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
+}
+def days(day_number):
+    day = weekdays[day_number]
+    return day
+    
+print(days(5))
