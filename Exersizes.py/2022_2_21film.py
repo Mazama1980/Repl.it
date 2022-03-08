e8619eb0f01d0cae02145b46c7111ec4876df33f
Mazama1980 <74572218+Mazama1980@users.noreply.github.com>
2022-03-07 19:38:05 -0700

    2022-3-7 Films project

--- Exersizes.py/2022_2_21film.py
+++ Exersizes.py/2022_2_21film.py
@@ -1,3 +1,5 @@
+"""https://github.com/siporter43/mainpypet.py/blob/master/MovieList.py; https://github.com/siporter43/mainpypet.py/blob/master/MovieList.py
+"""
 films = [
     "Pinocchio",
     "Dumbo",
@@ -101,6 +103,43 @@ for i, item in enumerate(films, 1):
 
 
 
+def film_review():
+    # list the movies
+    for i, item in enumerate(films, 1):
+        print(i, item)
+    
+    # ask the user
+    # user = index number of movie (i) and the place number
+    user = input("Type the number of your favorite movie.")
+    number_of_films = len(films)
+    
+    # [x] B. Make sure it is a movie in the list
+    if int(user) > 0 and int(user) <= number_of_films:
+        print(user)
+    else:
+        print("That movie isn't there, friends. Try again")
+        return
+    answer = int(user) -1
+    if answer < number_of_films:
+        # [x] C. Print the name of the movie that they picked
+        print(f'You picked the movie {films[answer]}.')
+    
+    place = input("What place in your favorites list would you like to put it?")
+    # [x] D. Move the movie to the new position
+    new_place = int(place)
+    films.insert(new_place, films[answer])
+    del films[int(user)]
+    
+    # [x] E. Print the list again in order with numbers next to each movie
+    for i, item in enumerate(films, 1):
+        print(i, item)
+
+
+
+"""""""""We need to ask:
+    What place would you like to move a movie in your favorites list? Input movie number, Place on user List
+        ex. (4, 1) fourth movie in 1st spot... Needs to iterate through list...user input(Are you finished with list?)
+        ex. if yes then print list, if no then Continue"""
 
 
 
