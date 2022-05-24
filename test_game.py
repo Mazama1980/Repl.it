49ed0d8513cc5f85f993d547be55005cc2bb82c8
Mazama1980 <74572218+Mazama1980@users.noreply.github.com>
2022-05-23 19:13:25 -0600

    2022-5-23 Given, When, Then for test functions
    Co-authored-by: Alissa Huskey <alissa.huskey@gmail.com>

--- test_game.py
+++ test_game.py
@@ -27,49 +27,97 @@ def teardown(request):
     """Auto-add teardown method to all tests."""
     request.addfinalizer(revert)
 
+# GIVEN: some context
+# WHEN: some action
+# THEN: some result
+
 
 def test_truth():
     assert True
 
 
 def test_is_for_sale():
+    # GIVEN: an item with a price
     fake_item = {
         "name": "art",
         "price": -20
     }
+
+    # WHEN: you call is_for_sale()
     result = is_for_sale(fake_item)
+
+    # THEN: it should return True
     assert result, "is_for_sale() should return True if the item has a price"
 
 def test_inventory_change():
+    # Given: Item and quantity Player's inventory
     adventure.PLAYER["inventory"]["lembas"] = 99
+
+    # When: call inventory_change for item with no quantity argument
     inventory_change("lembas")
+
+    # Then: should add one to quantity to Player's inventory
     assert adventure.PLAYER["inventory"]["lembas"] == 100, f'inventory_change() with no quantity argument shoud add 1.'
 
 def test_inventory_change_with_quantity():
+    # Given: Item and quantity to Player's inventory
     adventure.PLAYER["inventory"]["lembas"] = 99
+
+    # When: call inventory_change with item and specific quantity
     inventory_change("lembas", 2)
+
+    # Then: should add the specific quantity to the Player's inventory of that item
     assert adventure.PLAYER["inventory"]["lembas"] == 101
 
 def test_inventory_change_missing_key():
+    # Given: The Player inventory does not contain an item
     adventure.PLAYER["inventory"] = {}
+
+    # When: call inventory_change with item
     inventory_change("lembas")
-    # breakpoint()
+
+    # Then: Should add missing key of the item to the Player's inventory
     assert "lembas" in adventure.PLAYER["inventory"], f"inventory_change() should add missing keys to the inventory."
+
+    # And: Should add 1 to Player's inventory 
     assert adventure.PLAYER["inventory"]["lembas"] == 1, f"inventory_change() with no quantity argument should add 1."
     
 def test_inventory_change_remove():
+    # Given: Item and quantity in Player's inventory
     adventure.PLAYER["inventory"]["lembas"] = 5
+
+    # When: calling inventory_change with item and a negative quantity
     inventory_change("lembas", -6)
+
+    # Then: Should remove quantity of item from Player's inventory
     assert "lembas" not in adventure.PLAYER["inventory"], f"inventory_change() subtracting quantity <= 0 will remove key or item."
 
 def test_do_take():
-    # breakpoint()
+    # Given: Item and quantity in Player's inventory
     adventure.PLAYER["inventory"]["sword"] = 1
+
+    # And: item is possible to take
     adventure.ITEMS["sword"] = {"name":"short sword", "can_take": True}
+
+    # And: item is in the correct place
     adventure.PLACES["home"]["items"] = ["sword"]
+
+    # When: call do_take item
     do_take(["sword"])
+
+    # Then: Should have two of item in Player's inventory
     assert adventure.PLAYER ["inventory"]["sword"] == 2, f"Player inventory should have a sword added to total 2 sword"
+
+    # And: The item in that place should not be in that place anymore
     assert adventure.PLACES ["home"]["items"] == [], "The items list should be empty when the sword is taken"
+
+def test_do_drop():
+    # Given: Item in Player's inventory
+    # When: call do_drop with item
+    # Then: item should be gone from Player's inventory
+    # And: item should be added to the place where the Player is currently
+    ...
+
 
 def test_teardown():
     assert "lembas" not in adventure.PLAYER["inventory"], \
